972ed645743a516f3155853223ab4fa7ab2530ce
jmccreig <jmccreig@cisco.com>
2015-07-08 13:12:16 -0700

    update

--- Command/GenerateCommand.php
+++ Command/GenerateCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Ruudy\MetronicBundle\Command;
+namespace CoreSys\CommonBundle\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
