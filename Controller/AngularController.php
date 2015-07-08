58c73199a66ab20b6564f7567418f043cd889290
jmccreig <jmccreig@cisco.com>
2015-07-08 13:39:15 -0700

    update

--- Controller/AngularController.php
+++ Controller/AngularController.php
@@ -17,7 +17,6 @@ class AngularController extends Controller
     /**
      * @Route("/app/{pid}", name="common_angular_app", defaults={"pid"=null,"dependencies"=null})
      * @Route("/app/{pid}/{dependencies}", name="common_angular_app_dependencies", defaults={"pid"=null,"dependencies"=null})
-     * @Template()
      */
     public function appAction( $pid, $dependencies )
     {
@@ -26,7 +25,6 @@ class AngularController extends Controller
         }
         header( 'Content-Type: application/javascript' );
         echo $this->renderView( 'CoreSysCommonBundle:Angular:app.js.twig', array( 'pid' => $pid, 'dependencies' => json_encode( $dependencies ) ) );
-
-        return;
+        exit;
     }
 }
